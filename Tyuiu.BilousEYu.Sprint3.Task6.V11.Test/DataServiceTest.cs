cc9b32adedf3808b7310c2038422d3fa9f51a9de
Bilous.liza@bk.ru <Bilous.liza@bk.ru>
2024-10-29 18:53:59 +0500

    Task6

---
+++ Tyuiu.BilousEYu.Sprint3.Task6.V11.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.BilousEYu.Sprint3.Task6.V11.Lib;
+namespace Tyuiu.BilousEYu.Sprint3.Task6.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+            int startValue = 10;
+            int stopValue = 19;
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 9;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BilousEYu.Sprint3.Task6.V11/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.BilousEYu.Sprint3.Task6.V11.Lib;
+namespace Tyuiu.BilousEYu.Sprint3.Task6.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            
+            Console.Title = "Спринт #3 | Выполнила: Билоус Е.Ю. | ИСТНб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Организация ввода\\вывода в консольных приложениях                *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнила: Билоус Елизавета Юрьевна | ИСТНб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*Напишите программу, которая ищет среди целых чисел, принадлежащих        *");
+            Console.WriteLine("*числовому отрезку [10, 19] количество всех делителей больше 10           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+
+
+            int startValue = 10;
+            int stopValue = 19;
+
+            Console.WriteLine("Начало отрезка = " + startValue);
+            Console.WriteLine("Конец отрезка = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Количество делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+
+            Console.ReadKey();
+
+        }
+    }
+}
